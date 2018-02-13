dd5f4ab12d4e06f246c023f8705f184d5c16d763
erax01 <35724629+erax01@users.noreply.github.com>
2018-02-13 11:29:18 +0100

    Update Test.java
    Edited by Jordi

--- Test.java
+++ Test.java
@@ -1,7 +1,10 @@
 public class HelloPeople {
 	
-	
-	public static void main(String[] args) {	
-		
+	public static void Jordi() {
+		System.out.println("Emre ist ein cooler Dude");
 	}
+	public static void main(String[] args) {	
+		Jordi();
+	}
+	 
 ]
